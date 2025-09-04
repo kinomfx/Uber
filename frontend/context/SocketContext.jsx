46e3af75b1df11abd98e56c92e73aad59d3b24b3
kinom_fx <monikrayu@gmail.com>
2025-09-04 13:47:37 +0530

    frontend done

--- frontend/context/SocketContext.jsx
+++ frontend/context/SocketContext.jsx
@@ -4,7 +4,11 @@ import { io } from 'socket.io-client';
 
 export const SocketContext = createContext();
 
-const socket = io(`${import.meta.env.VERCEL_BASE_URL}`); // Replace with your server URL
+const socket = io(import.meta.env.VITE_BACKEND_URL, {
+  transports: ["polling"],  // force long-polling
+  withCredentials: true
+});
+
 
 const SocketProvider = ({ children }) => {
     useEffect(() => {
